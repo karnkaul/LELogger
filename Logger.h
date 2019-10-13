8ca62b8719512e6ccea6435baad68ed0bca7d108
Karn Kaul <karnkaul@gmail.com>
2019-10-12 23:30:47 -0700

    Clang-format pass

--- Logger.h
+++ Logger.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * == LittleEngine Logger ==
  *   Copyright 2019 Karn Kaul
  * Features:
@@ -21,13 +21,29 @@
 #define LOG_E(x, ...) LOG_SEVERITY(x, Error, ##__VA_ARGS__)
 #define LOG_W(x, ...) LOG_SEVERITY(x, Warning, ##__VA_ARGS__)
 #define LOG_I(x, ...) LOG_SEVERITY(x, Info, ##__VA_ARGS__)
-#define LOGIF_E(bCond, x, ...) if (bCond) { LOG_SEVERITY(x, Error, ##__VA_ARGS__); }
-#define LOGIF_W(bCond, x, ...) if (bCond) { LOG_SEVERITY(x, Warning, ##__VA_ARGS__); }
-#define LOGIF_I(bCond, x, ...) if (bCond) { LOG_SEVERITY(x, Info, ##__VA_ARGS__); }
+#define LOGIF_E(bCond, x, ...)                 \
+	if (bCond)                                 \
+	{                                          \
+		LOG_SEVERITY(x, Error, ##__VA_ARGS__); \
+	}
+#define LOGIF_W(bCond, x, ...)                   \
+	if (bCond)                                   \
+	{                                            \
+		LOG_SEVERITY(x, Warning, ##__VA_ARGS__); \
+	}
+#define LOGIF_I(bCond, x, ...)                \
+	if (bCond)                                \
+	{                                         \
+		LOG_SEVERITY(x, Info, ##__VA_ARGS__); \
+	}
 
 #if defined(DEBUG_LOGGING) && DEBUG_LOGGING
 #define LOG_D(x, ...) LOG_SEVERITY(x, Debug, ##__VA_ARGS__)
-#define LOGIF_D(bCond, x, ...) if (bCond) { LOG_SEVERITY(x, Debug, ##__VA_ARGS__);  }
+#define LOGIF_D(bCond, x, ...)                 \
+	if (bCond)                                 \
+	{                                          \
+		LOG_SEVERITY(x, Debug, ##__VA_ARGS__); \
+	}
 #else
 #define LOG_D(x, ...)
 #define LOGIF_D(bCond, x, ...)
